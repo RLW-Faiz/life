e21bf2c7ef6498f54c55430d9ee0fecc5a5708e2
unknown <2653662312@qq.com>
2020-06-22 19:54:08 +0800

    add hot plate

--- pages/index/index.js
+++ pages/index/index.js
@@ -17,21 +17,27 @@ Page({
       },
     ]
   },
+  // 按钮事件
   issue(e){
     let index =e.currentTarget.dataset.status,
     url2 = '/pages/releaseTopic/releaseTopic',  // 发布话题
     url1 ='', //收藏的模板
     url = ''; //申请模板
     wx.navigateTo({
+      url: index == 2? url2 :index == 1? url1 :url,
+    })
+  },
+  // 内容获取跳转事件
+  get_content(e){
+    let index =e.currentTarget.dataset.status,
+    url2 = '/pages/topicContent/topicContent',
+    url1 = '',
+    url = '/pages/experienceList/experienceList';
+    wx.navigateTo({
       url: index == 2? url2:index == 1? url1 :url,
     })
   },
-  get_hot_topic(){
-    let url = '/pages/topicContent/topicContent'
-    wx.navigateTo({
-      url,
-    })
-  },
+  //更多按钮跳转事件
   get_more(e){
     let index =e.currentTarget.dataset.status,
     url2 = '/pages/topicList/topicList',// 发布话题
